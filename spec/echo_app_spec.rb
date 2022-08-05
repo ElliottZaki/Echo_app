28a47edf1c61e923369a0b33c07aa35373c58940
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-08-05 17:17:52 +0100

    Refractored tests for readability - Created a let Time.new.

--- spec/echo_app_spec.rb
+++ spec/echo_app_spec.rb
@@ -1,7 +1,9 @@
 require './lib/echo_app.rb'
 
 describe Echo_app do
-  let(:time) {Time.new(time.strftime("%d/%m/%Y") + (" | ") + time.strftime("%k:%M") + (" | "))} 
+  # let(:time) {Time.new(time.strftime("%d/%m/%Y") + (" | ") + time.strftime("%k:%M") + (" | "))} 
+  let(:time) {Time.new}
+  let(:time_format) {(time.strftime("%d/%m/%Y") + (" | ") + time.strftime("%k:%M") + (" | "))} 
 
   describe '#promt_input' do
     specify { expect { subject.prompt_input }.to output("Say somthing:\n").to_stdout } 
@@ -9,14 +11,21 @@ describe Echo_app do
 
   describe '#return_time' do
     it 'returns the current time' do
-      time = Time.new
-      expect(subject.return_time).to eq (time.strftime("%d/%m/%Y") + (" | ") + time.strftime("%k:%M") + (" | "))
+      expect(subject.return_time).to eq (time_format)
     end
   end
   
   describe '#echo' do
-    time = Time.new
-    specify { expect { subject.echo }.to output("Say somthing:\n#{time.strftime("%d/%m/%Y") + (" | ") + time.strftime("%k:%M") + (" | ")}""You said: \n").to_stdout } 
+    specify { expect { subject.echo }.to output("Say somthing:\n#{time_format}""You said: \n").to_stdout } 
   end 
 
 end
+
+
+#  # let(:time) {Time.new(time.strftime("%d/%m/%Y") + (" | ") + time.strftime("%k:%M") + (" | "))} 
+#  let(:time) { double(Time.new) }
+#  # let(:time) {Time.new}
+#  var = double(time(time.strftime("%d/%m/%Y") + (" | ") + time.strftime("%k:%M") + (" | ")))
+#  # let(:time) {time.strftime("%d/%m/%Y") + (" | ") + time.strftime("%k:%M") + (" | ")} 
+
+#  describe '#promt_input' do
